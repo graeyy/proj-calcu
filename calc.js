23bd9575b1145a892cb8e8ee092b18fc44169651
graeyy <sseureki02@gmail.com>
2020-11-02 08:57:31 +0800

    added keyboard support + fixed design

--- calc.js
+++ calc.js
@@ -1,4 +1,7 @@
-
+/**
+ * TODO: add keyboard support 
+ * * limit number display
+ */
 
 const btnNums = document.querySelectorAll('.num');
 const screen = document.getElementById('screen');
@@ -6,44 +9,88 @@ var activeSign = '';
 var total = null;
 var firstNum = true;
 var num;
+var withDec = false;
+
+document.addEventListener('keydown', function(e) {
+    if (e.key >= 0 && e.key <= 9 || e.key == '.') {
+        addNum(e.key);
+    }
+    if (e.key == 'Backspace') {
+        delChar();
+    }
+
+    if (e.key == '+' || e.key == '−' || e.key == '÷' || e.key == '×') {
+        activateSign();
+        activeSign = e.key;
+    }
+    if (e.key == '=' || e.key == 'Enter') {
+        equals();
+    }
+})
+
 btnNums.forEach(button => {
     button.addEventListener('click', () => {
         num = button.textContent;
-        if (firstNum && num != '.') {
+        addNum(num);
+    })
+})
+
+function addNum(num) {
+    //num = e.target.textContent;
+    /**
+     * TODO: refactor this pls, redundant
+     */
+    if (num == '.') {
+        if (!withDec) {
+            withDec = true;
+            if (firstNum) {
+                screen.textContent = '0' + num;
+            } else {
+                screen.textContent += num;
+            }
+        }
+    } else {
+        if (firstNum) {
             screen.textContent = num;
             //console.log('if')
         } else {
             screen.textContent += num;
             //console.log('else')
         }
+    }
 
-        firstNum = false;
-        
-        //console.log('pressed: ' + button.textContent);
-        //console.log('activeSign: ' + activeSign);
-    })
-})
+    firstNum = false;
+    
+    //console.log('pressed: ' + button.textContent);
+    //console.log('activeSign: ' + activeSign);
+}
 
 const btnSigns = document.querySelectorAll('.sign');
 
 btnSigns.forEach(button => {
     button.addEventListener('click', () => {
-        if (activeSign == '') {
-            if (total == null) {
-                num = parseFloat(screen.textContent);
-                total = num;
-                //console.log(typeof total)
-                //console.log('total: ' + total + ', activeSign: ' + activeSign)
-            }
-        } else {
-            calculate();
-        }
-        
+        activateSign();
         activeSign = button.textContent;
-        //console.log('activeSign: ' + activeSign);
-        firstNum = true;
     })
 })
+
+function activateSign() {
+    if (activeSign == '') {
+        if (total == null) {
+            num = parseFloat(screen.textContent);
+            total = num;
+            //console.log(typeof total)
+            //console.log('total: ' + total + ', activeSign: ' + activeSign)
+        }
+    } else {
+        calculate();
+    }
+    
+    //console.log('activeSign: ' + activeSign);
+    firstNum = true;
+
+    withDec = false;
+}
 
 function calculate() {
     num = parseFloat(screen.textContent);
@@ -51,27 +98,31 @@ function calculate() {
     if (activeSign == '+') {
         total += num;
     }
-    else if (activeSign == '-') {
+    else if (activeSign == '−') {
         total -= num;
     }
-    else if (activeSign == '/') {
+    else if (activeSign == '÷') {
         total /= num;
     }
-    else if (activeSign == 'x') {
+    else if (activeSign == '×') {
         total *= num;
     } else {
         total = num;
     }
     console.log(total);
+    screen.textContent = total;
+    withDec = false;
 }
 
 function equals() {
     calculate();
-    screen.textContent = total;
+    //screen.textContent = total;
 
-    total = parseFloat(screen.textContent);
+    total = null;
+    //total = parseFloat(screen.textContent);
     firstNum = true;
     activeSign = '';
+    withDec = false;
 }
 
 const btnEquals = document.getElementById('op-eq');
@@ -79,7 +130,7 @@ const btnEquals = document.getElementById('op-eq');
 btnEquals.addEventListener('click', equals);
 
 const btnAllClr = document.getElementById('op-allClr');
-const btnClr = document.getElementById('op-clr');
+const btnDel = document.getElementById('op-del');
 const btnSign = document.getElementById('op-sign');
 
 btnAllClr.addEventListener('click', () => {
@@ -87,15 +138,40 @@ btnAllClr.addEventListener('click', () => {
     screen.textContent = 0;
     firstNum = true;
     activeSign = '';
+    withDec = false;
 })
 
-btnClr.addEventListener('click', () => {
-    screen.textContent = 0;
-    firstNum = true;
-})
+btnDel.addEventListener('click', delChar)
+
+function delChar() {
+    num = screen.textContent;
+
+    if (num.length > 1) {
+        var n = num.substr(num.length-1, 1);
+        if (n == '.') withDec = false;
+        num = num.substr(0, num.length - 1);
+    } else {
+        num = 0;
+    }
+    
+    screen.textContent = num;
+
+}
 
 btnSign.addEventListener('click', () => {
     num = parseFloat(screen.textContent);
     num *= -1;
     screen.textContent = num;
 })
+
+/*
+const decPoint = document.getElementById('op-dec');
+
+decPoint.addEventListener('click', () => {
+    
+    if (!withDec) {
+        screen.textContent += '.';
+        withDec = true;
+        firstNum = false;
+    }
+})*/
